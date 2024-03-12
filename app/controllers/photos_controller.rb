3dc6b44b3936749fd9e36223798902650b6718f0
crystal tliourane <129141063+ctlio@users.noreply.github.com>
2024-03-12 17:15:01 +0000

    ensure_user_is_authorized to photos_controller and pundit not authorized error message

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,7 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: %i[ show edit update destroy ]
   before_action :ensure_current_user_is_owner, only: [:destroy, :update, :edit]
+  before_action :ensure_user_is_authorized, only: [:show]
 
   # GET /photos or /photos.json
   def index
@@ -71,9 +72,14 @@ class PhotosController < ApplicationController
       end
     end
 
+    def ensure_user_is_authorized
+      if !PhotoPolicy.new(current_user, @photo).show?
+        raise Pundit::NotAuthorizedError, "not allowed"
+      end
+    end
+
     # Only allow a list of trusted parameters through.
     def photo_params
       params.require(:photo).permit(:image, :comments_count, :likes_count, :caption, :owner_id)
     end
 end
-
